fea59ed9a46aa40b3cd4c20c2a80fd8c74aab14b
aaush99 <aaushshrestha1012gmail.com>
2025-06-06 11:05:43 +0545

    bookforyou page added

--- src/bookforyou/BookForYou.java
+++ src/bookforyou/BookForYou.java
@@ -3,7 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package bookforyou;
-
+import View.SignUp;
+import controller.SignUpController;
 /**
  *
  * @author Nitro
@@ -15,6 +16,10 @@ public class BookForYou {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        SignUp signup = new SignUp();
+        SignUpController contr = new SignUpController(signup);
+        contr.open();
     }
     
 }
