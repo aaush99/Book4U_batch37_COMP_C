8db499e4df6f4921be4ade675d22d41ba256e51e
aaush99 <aaushshrestha1012gmail.com>
2025-06-09 15:19:39 +0545

    changes made

---
+++ src/Model/ProductModel.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author Nitro
+ */
+public class ProductModel
+{
+    private String productName;
+    private String productImage;
+    private int productPrice;
+
+    public ProductModel(String productName, String productImage, int productPrice) 
+    {
+        this.productName = productName;
+        this.productImage = productImage;
+        this.productPrice = productPrice;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductImage() {
+        return productImage;
+    }
+
+    public void setProductImage(String productImage) {
+        this.productImage = productImage;
+    }
+
+    public int getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(int productPrice) {
+        this.productPrice = productPrice;
+    } 
+}
