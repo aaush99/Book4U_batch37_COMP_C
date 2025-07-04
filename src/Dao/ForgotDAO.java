a5d72a7d5ae22290acbd0eeb0e2306f195a17abe
ofcourse-its-Shraddha <240599@softwarica.edu.np>
2025-07-04 18:49:18 +0545

    forgotdao

---
+++ src/Dao/ForgotDAO.java
@@ -0,0 +1,251 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+//mport Database.Mysql;
+import Database.Mysql;
+import Model.ForgotModel;
+//import Model.SecAnswers;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ForgotDAO {
+    Mysql mysql = new Mysql() {
+        @Override
+        public Object open() {
+            return null;
+        }
+    };
+
+    // ✅ New method to insert answers
+ public void insertSecurityAnswers(ForgotModel user) {
+    Connection conn = mysql.openConnection(); 
+    String query = "INSERT INTO user (Email, answer1, answer2, answer3) VALUES (?, ?, ?, ?)";
+    
+    try (PreparedStatement stmt = conn.prepareStatement(query)) {
+        stmt.setString(1, user.getEmail());
+        stmt.setString(2, user.getanswer1());
+        stmt.setString(3, user.getanswer2());
+        stmt.setString(4, user.getanswer3());
+        
+        stmt.executeUpdate();
+        System.out.println("Security answers inserted successfully for: " + user.getEmail());
+    } catch (SQLException e) {
+        e.printStackTrace();
+    } finally {
+        mysql.closeConnection(conn);
+    }
+}
+
+
+
+    public boolean validateSecurityAnswers(String email, String[] answers) {
+        if(email == null || answers == null )return false;
+        Connection conn = mysql.openConnection();
+        String query = "SELECT answer1, answer2, answer3 FROM user WHERE Email = ?";
+        try (PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setString(1, email);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                boolean match1 = rs.getString("answer1").equalsIgnoreCase(answers[0]);
+                boolean match2 = rs.getString("answer2").equalsIgnoreCase(answers[1]);
+                boolean match3 = rs.getString("answer3").equalsIgnoreCase(answers[2]);
+                return match1 && match2 && match3;
+            }
+        } catch (SQLException e) {
+            Logger.getLogger(ForgotDAO.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            mysql.closeConnection(conn);
+        }
+        return false;
+    }
+
+    public boolean updatePassword(String email, String newPassword) {
+        if (email == null || newPassword == null)return false;
+        Connection conn = mysql.openConnection();
+        String query = "UPDATE user SET set_password = ?, confirm_password = ? WHERE Email = ?";
+        try (PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setString(1, newPassword);
+            stmt.setString(2, newPassword);
+            stmt.setString(3, email);
+            return stmt.executeUpdate() > 0;
+        } catch (SQLException e) {
+            Logger.getLogger(ForgotDAO.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            mysql.closeConnection(conn);
+        }
+        return false;
+    }
+    public boolean emailExists(String email){
+        Connection conn = mysql.openConnection();
+        String sql = "SELECT Email from user WHERE Email = ?";
+        try(PreparedStatement pstmt = conn.prepareStatement(sql)){
+            pstmt.setString(1,email);
+            ResultSet rs = pstmt.executeQuery();
+            return rs.next();
+            
+        }catch(SQLException e){
+            e.printStackTrace();
+            return false;
+        }
+    }
+    
+    public boolean settingsPassUpdate(String email, String changePass) {
+        if (email == null || changePass == null)return false;
+        Connection conn = mysql.openConnection();
+        String query = "UPDATE user SET set_password = ?, confirm_password = ? WHERE Email = ?";
+        try (PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setString(1, changePass);
+            stmt.setString(2, changePass);
+            stmt.setString(3, email);
+            return stmt.executeUpdate() > 0;
+        } catch (SQLException e) {
+            Logger.getLogger(ForgotDAO.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            mysql.closeConnection(conn);
+        }
+        return false;
+    } 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//package DAO;
+//import Database.Database;
+//import java.sql.*;
+//import Database.MySqlConnection;
+//import Model.SecAnswers;
+//import java.util.logging.Level;
+//import java.util.logging.Logger;
+///**
+// *
+// * @author LEGION
+// */
+//public class AuthDao {
+//    MySqlConnection mysql = new MySqlConnection();
+////    private Connection conn;
+//    
+//    public void AuthDao(SecAnswers user){
+//         Connection conn = mysql.openConnection(); 
+//        String query = "INSERT INTO user_security (answer1,answer2,answer3)VALUES (?,?,?,?)";
+//        try(PreparedStatement stmt = conn.prepareStatement(query)){
+////            stmt.setString(1, user.getEmail());
+//            stmt.setString(2, user.getanswer1());
+//            stmt.setString(3,user.getanswer2());
+//            stmt.setString(4, user.getanswer3());
+//            
+//            stmt.executeUpdate();
+//        }catch(SQLException e){
+//            e.printStackTrace();
+////            Logger.getLogger(AuthDao.class.getName()).log(Level.SEVERE, null, ex)
+//        }finally {
+//            mysql.closeConnection(conn);
+//        }
+////        Connection conn = mysql.openConnection();
+////        conn = Database.getConnection();
+//    }
+////    public void insertAnswers(SecAnswers user){
+////        Connection conn = mysql.openConnection(); 
+////        String query = "INSERT INTO user_security (answer1,answer2,answer3)VALUES (?,?,?,?)";
+////        try(PreparedStatement stmt = conn.prepareStatement(query)){
+//////            stmt.setString(1, user.getEmail());
+////            stmt.setString(2, user.getanswer1());
+////            stmt.setString(3,user.getanswer2());
+////            stmt.setString(4, user.getanswer3());
+////            
+////            stmt.executeUpdate();
+////        }catch(SQLException e){
+////            e.printStackTrace();
+//////            Logger.getLogger(AuthDao.class.getName()).log(Level.SEVERE, null, ex)
+////        }finally {
+////            mysql.closeConnection(conn);
+////        }
+////    }
+//    
+//    public boolean validateSecurityAnswers(String email, String[] answers){
+//        Connection conn = mysql.openConnection();
+//        String query = "SELECT answer1, answer2, answer3 FROM user_security WHERE email = ?";
+//        try (PreparedStatement stmt = conn. prepareStatement(query)){
+//            stmt.setString(1, email);
+//            ResultSet rs = stmt.executeQuery();
+//            if (rs.next()){
+//                return rs.getString("answer1").equalsIgnoreCase(answers[0]) &&
+//                       rs.getString("answer2").equalsIgnoreCase(answers[1]) &&
+//                       rs.getString("answer3").equalsIgnoreCase(answers[2]);
+//            }
+//        } catch (SQLException e){
+//            Logger.getLogger(AuthDao.class.getName()).log(Level.SEVERE,null,e);
+////            e.printStackTrace();
+//        }
+//       finally {
+//            mysql.closeConnection(conn);
+//        }
+//        return false;
+//    }
+//    public boolean updatePassword(String email, String set_password){
+//        Connection conn = mysql.openConnection();
+//        String query = "UPDATE users SET set_password = ? WHERE email = ?";
+//        try (PreparedStatement stmt = conn. prepareStatement(query)){
+//            stmt.setString(1, set_password);
+//            stmt.setString(2, email);
+//            return stmt.executeUpdate()>0;
+//            
+//        } catch (SQLException e){
+//            e.printStackTrace();
+//        }
+//        return false;
+//    }
+//}
